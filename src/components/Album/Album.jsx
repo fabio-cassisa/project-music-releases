71e8f569393d7f9b7a2d59d5d0162f55ceb9c303
Fabio <cassisafabio97@gmail.com>
2023-10-19 01:31:29 +0200

    responsiveness working good, css and style are now good, links working

--- src/components/Album/Album.jsx
+++ src/components/Album/Album.jsx
@@ -9,8 +9,8 @@ import "../../styles/Album.css";
 export const Album = ({ albumData }) => {
   return (
     <div className="album">
-      <CoverImage imageUrl={albumData.images[0].url} />
-      <div className="album-overlay">
+      <div className="cover-wrapper">
+        <div className="album-overlay">
         <button className="favorite-button">
           <img src={FavoriteIcon} alt="Favorite" />
         </button>
@@ -20,6 +20,8 @@ export const Album = ({ albumData }) => {
         <button className="more-button">
           <img src={MoreIcon} alt="More" />
         </button>
+      </div>
+      <CoverImage imageUrl={albumData.images[0].url} />
       </div>
       <AlbumName name={albumData.name} url={albumData.external_urls.spotify} />
       <ArtistName artists={albumData.artists} />
