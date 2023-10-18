1be22543d24691a1d6f40c622bada7026e7bc7c5
Calleobe <145662831+Calleobe@users.noreply.github.com>
2023-10-18 15:48:15 +0200

    Update base structure and css #2

---
+++ src/components/Album/Header.jsx
@@ -0,0 +1,8 @@
+import "../../styles/Header.css";
+
+export const Header = () => (
+  <header>
+    <h1 className="header">Music Albums</h1>
+    <p>Made by Carl and Fabio </p>
+  </header>
+);
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,17 @@
 import data from "../data.json";
 import { Album } from "./Album/Album";
+import { Header } from "./Album/Header";
 import "../styles/Album.css";
 
 export const App = () => {
   return (
-    <section className="album-container">
-      {data.albums.items.map((album) => (
-        <Album key={album.id} albumData={album} />
-      ))}
-    </section>
+    <>
+      <section className="album-container">
+        <Header />
+        {data.albums.items.map((album) => (
+          <Album key={album.id} albumData={album} />
+        ))}
+      </section>
+    </>
   );
 };
