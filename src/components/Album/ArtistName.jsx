18d790391888a4cda1afcae9e57d5cae46fefa06
Calleobe <145662831+Calleobe@users.noreply.github.com>
2023-10-19 11:10:49 +0200

    , and & between artists

--- src/components/Album/ArtistName.jsx
+++ src/components/Album/ArtistName.jsx
@@ -1,16 +1,17 @@
 import "../../styles/ArtistName.css";
 
 export function ArtistName({ artists }) {
-  const artistNames = artists.map((artist) => artist.name);
-  //const displayNames = artistNames.join(", ").replace(/, ([^,]*)$/, " & $1");
-
   return (
     <div className="artist-names">
       {artists.map((artist, index) => (
-        <h3 key={artist.id}>
+        <span className="separator" key={artist.id}>
           <a href={artist.external_urls.spotify}>{artist.name}</a>
-          {index !== artists.length - 1 && ", "}
-        </h3>
+          {index === artists.length - 2
+            ? " & "
+            : index !== artists.length - 1
+            ? ", "
+            : ""}
+        </span>
       ))}
     </div>
   );
