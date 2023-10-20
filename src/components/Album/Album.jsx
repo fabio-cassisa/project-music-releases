804f761d91e4f90e65ca677245a7676cdc714350
Calleobe <145662831+Calleobe@users.noreply.github.com>
2023-10-20 13:34:36 +0200

    first changes

--- src/components/Album/Album.jsx
+++ src/components/Album/Album.jsx
@@ -4,24 +4,28 @@ import { CoverImage } from "./CoverImage";
 import PlayIcon from "../../assets/icons/play.svg";
 import FavoriteIcon from "../../assets/icons/heart.svg";
 import MoreIcon from "../../assets/icons/dots.svg";
+import InfoIcon from "../../assets/icons/info-icon.svg";
 import "../../styles/Album.css";
 
 export const Album = ({ albumData }) => {
   return (
     <div className="album">
       <div className="cover-wrapper">
-        <div className="album-overlay">
-        <button className="favorite-button">
+        <button className="info-button">
           <img src={FavoriteIcon} alt="Favorite" />
         </button>
-        <button className="play-button">
-          <img src={PlayIcon} alt="Play" />
-        </button>
-        <button className="more-button">
-          <img src={MoreIcon} alt="More" />
-        </button>
-      </div>
-      <CoverImage imageUrl={albumData.images[0].url} />
+        <div className="album-overlay">
+          <button className="favorite-button">
+            <img src={FavoriteIcon} alt="Favorite" />
+          </button>
+          <button className="play-button">
+            <img src={PlayIcon} alt="Play" />
+          </button>
+          <button className="more-button">
+            <img src={MoreIcon} alt="More" />
+          </button>
+        </div>
+        <CoverImage imageUrl={albumData.images[0].url} />
       </div>
       <AlbumName name={albumData.name} url={albumData.external_urls.spotify} />
       <ArtistName artists={albumData.artists} />
